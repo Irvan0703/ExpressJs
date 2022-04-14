8770bf7915cc52a577316ebfdc933abe484345b9
Irvan0703 <muhamadirfanulhadi@gmail.com>
2022-04-14 11:25:22 +0700

    add data

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,18 @@ router.get('/', (req, res)=>{
         status:'Successfully',
         message: 'Selamat datang di Express Js',
         view:'Jika ingin menampilkan gambar maka pada url harus ditambah  /public/pic.jpg',
-        data: 'Jika ingin menampilkan data maka setelah url harus ditambah /api/v3/product'
+        data: 'Jika ingin menampilkan data maka setelah url harus ditambah /data'
+    })
+})
+
+router.get('/data', (req, res)=>{
+    res.send({
+        status:'Successfully',
+        "_id": "625635b7570781d72f3719c2",
+		"name": "Laptop Asus",
+		"price": 14000000,
+		"stock": 10,
+		"status": true
     })
 })
 
