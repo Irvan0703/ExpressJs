eb155c38c29c65dccd8c86eb6fcdbd9437e35d83
Irvan0703 <muhamadirfanulhadi@gmail.com>
2022-04-17 16:36:15 +0700

    Back End Integrasi

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const path = require('path');
 const productRouterV3 = require('./app/products_v3/routes');
 const productRouterV4 = require('./app/products_v4/routes');
 const logger = require('morgan');
-const router = require('./app/routes')
+const router = require('./app/routes');
+const cors = require('cors');
 
 app.use(logger('dev'));
 app.use(express.urlencoded({extended: true}));
@@ -15,10 +16,11 @@ app.use(express.json());
 
 app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use(router)
+app.use(cors())
 //app.use('/api/v1', productRouter);
 //app.use('/api/v2', productRouterV2);
-app.use('/api/v3', productRouterV3);
-app.use('/api/v4', productRouterV4);
+app.use('/api/v1', productRouterV3);
+app.use('/api/v2', productRouterV4);
 app.use((req,res, next) => {
     res.status(404);
     res.send({
