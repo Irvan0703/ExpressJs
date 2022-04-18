684e962aa8d57813e4c515912d6469369466026a
Irvan0703 <muhamadirfanulhadi@gmail.com>
2022-04-18 08:50:26 +0700

    Express Js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require('./config/mongoose')
 const express = require('express');
 const app = express();
 const path = require('path');
-const productRouterV3 = require('./app/products_v3/routes');
-const productRouterV4 = require('./app/products_v4/routes');
+//const productRouterV3 = require('./app/products_v3/routes');
+//const productRouterV4 = require('./app/products_v4/routes');
 const logger = require('morgan');
 const router = require('./app/routes');
 const cors = require('cors');
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use('/public', express.static(path.join(__dirname, 'uploads')));
 app.use(router)
 app.use(cors());
-app.use('/api/v1', productRouterV3);
-app.use('/api/v2', productRouterV4);
+//app.use('/api/v1', productRouterV3);
+//app.use('/api/v2', productRouterV4);
 app.use((req,res, next) => {
     res.status(404);
     res.send({
